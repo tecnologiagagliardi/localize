7ed90ab65160b6f711a47d07aa7ca34f3fc9b67b
Tecnologia Gagliardi <informatica@grupogagliardi.com>
2026-02-10 18:06:19 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   const clientCodeInput = document.getElementById('client-code');
-  const clientReferenceInput = document.getElementById('clientreference');
   const capturePhotoButton = document.getElementById('capture-photo');
   const restartProcessButton = document.getElementById('restart-process');
   const photoPreview = document.getElementById('photo-preview');
@@ -10,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const mapTitle = document.getElementById('map-title');
   const phoneInput = document.getElementById('phone');
   const emailInput = document.getElementById('email');
+  const referencePointInput = document.getElementById('reference-point');
 
   let clientCode = '';
   let photoBlob = null;
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
     clientCode = sanitizeClientCode(clientCodeInput.value);
     const phone = sanitizePhone(phoneInput.value);
     const email = emailInput.value;
-    const clientreference = clientReferenceInput.value;
+    const referencePoint = referencePointInput.value.trim();
 
     if (!validateClientCode(clientCode)) {
       alert('Código Cliente inválido!\n\nExemplo: C000001');
@@ -105,7 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <strong>Código Cliente:</strong> ${clientCode}<br>
         <strong>Tel.:</strong> ${phone || 'Não fornecido'}<br>
         <strong>E-mail:</strong> ${email || 'Não fornecido'}<br>
-        <strong>Ponto de referência</strong> ${clientreference || 'Não fornecido'}<br>
+        <strong>Ponto de referência:</strong> ${referencePoint || 'Não informado'}<br>
         <strong>Latitude:</strong> ${locationData.latitude.toFixed(6)}<br>
         <strong>Longitude:</strong> ${locationData.longitude.toFixed(6)}<br>
       `;
@@ -145,7 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Compartilhar
   shareButton.addEventListener('click', async () => {
-    const textData = `Código Cliente: ${clientCode}\nTel.: ${phoneInput.value}\nE-mail: ${emailInput.value}\nLatitude: ${locationData.latitude.toFixed(6)}\nLongitude: ${locationData.longitude.toFixed(6)}`;
+    const textData = `Código Cliente: ${clientCode}\nTel.: ${phoneInput.value}\nE-mail: ${emailInput.value}\nPonto de referência: ${referencePointInput.value || 'Não informado'}\nLatitude: ${locationData.latitude.toFixed(6)}\nLongitude: ${locationData.longitude.toFixed(6)}`;
 
     try {
       await navigator.clipboard.writeText(textData);
